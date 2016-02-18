7a19b4c770c93bbafeb3fd8fca2a022fa2c1e4b6
James Cool <jeffreysbrother@gmail.com>
2016-02-18 12:58:54 -0800

    trying better alternative

--- custom.js
+++ custom.js
@@ -1,11 +1,26 @@
-$( document ).ready(function() {
-  $(".equalizer").each(function() {
-    var heights = $(this).find(".watch").map(function() {
-      return $(this).height();
-    }).get(),
+// $( document ).ready(function() {
+//
+//     $(".row-eq-height").each(function() {
+//       var heights = $(this).find(".col-eq-height").map(function() {
+//         return $(this).height();
+//       }).get(),
+//
+//       maxHeight = Math.max.apply(null, heights);
+//
+//       $(".col-eq-height").height(maxHeight);
+//     });
+//
+// });
 
-    maxHeight = Math.max.apply(null, heights);
 
-    $(".watch").height(maxHeight);
-  });
-}());
+
+
+// $(document).ready(function() {
+//     $(".row-eq-height").each(function() {
+//         var heights = $(this).find(".col-eq-height").map(function() {
+//             return $(this).outerHeight();
+//         }).get(), maxHeight = Math.max.apply(null, heights);
+//
+//         $(this).find(".col-eq-height").outerHeight(maxHeight);
+//     });
+// });
