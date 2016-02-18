ddac92ffd50f9fbd85914856c7c9385f78382a37
James Cool <jeffreysbrother@gmail.com>
2016-02-18 15:49:19 -0800

    update

--- custom.js
+++ custom.js
@@ -15,12 +15,12 @@
 
 
 
-// $(document).ready(function() {
-//     $(".row-eq-height").each(function() {
-//         var heights = $(this).find(".col-eq-height").map(function() {
-//             return $(this).outerHeight();
-//         }).get(), maxHeight = Math.max.apply(null, heights);
-//
-//         $(this).find(".col-eq-height").outerHeight(maxHeight);
-//     });
-// });
+$(document).ready(function() {
+    $(".row-eq-height").each(function() {
+        var heights = $(this).find(".col-eq-height").map(function() {
+            return $(this).outerHeight();
+        }).get(), maxHeight = Math.max.apply(null, heights);
+
+        $(this).find(".col-eq-height").outerHeight(maxHeight);
+    });
+});
