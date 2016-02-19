9471241f2aa5ce642d5b4590371307d8e3945e0a
James Cool <jeffreysbrother@gmail.com>
2016-02-19 13:14:24 -0800

    fixed formatting

--- README.md
+++ README.md
@@ -29,7 +29,6 @@ $(document).ready(function() {
   if ($(window).width() > 950) {
     $('.row-eq-height').each(function() {
       var eHeight = $(this).innerHeight();
-
       $(this).find('.col-eq-height').outerHeight(eHeight);
     });
   }
--- js-solution/custom.js
+++ js-solution/custom.js
@@ -1,27 +1,8 @@
-
-
-  $(document).ready(function() {
-    if ($(window).width() > 950) {
-
-  // SOLUTION #1
-    // $(".row-eq-height").each(function() {
-    //     var heights = $(this).find(".col-eq-height").map(function() {
-    //         return $(this).outerHeight();
-    //     }).get(), maxHeight = Math.max.apply(null, heights);
-    //
-    //     $(this).find(".col-eq-height").outerHeight(maxHeight);
-    // });
-
-
-
-
-
-  // //SOLUTION #2
+$(document).ready(function() {
+  if ($(window).width() > 950) {
     $('.row-eq-height').each(function() {
       var eHeight = $(this).innerHeight();
-
       $(this).find('.col-eq-height').outerHeight(eHeight);
     });
-
-}
+  }
 });
